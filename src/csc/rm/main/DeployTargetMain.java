74a56102f6fa97cbce75297648c21d1d322b1a45
zdk <85527792@qq.com>
2018-11-07 16:11:16 +0800

    1

--- src/csc/rm/main/DeployTargetMain.java
+++ src/csc/rm/main/DeployTargetMain.java
@@ -7,7 +7,7 @@ import csc.rm.rmi.RmiServiceConfig;
  * 功能描述:
  * Created by zdk on 2018/11/7.
  */
-public class DeployMain {
+public class DeployTargetMain {
 
     public static void main(String[] args) throws Exception {
         RmiServiceConfig.init();
